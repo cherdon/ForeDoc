9d2626451aa6daa2402796d988fd0f24ba8d7272
cherdon <liewcherdon@gmail.com>
2020-07-18 17:40:45 +0800

    Initial Documentation Structure

---
+++ docdata/modules_toc.js
@@ -0,0 +1,36 @@
+toc = {
+  "title": " Four Modules (1.0.1)",
+  "link": "FourModules",
+  "children": [
+    {
+      "title": " Procedural Building",
+      "link": "SettingUpVehicleSimul",
+      "children": null
+    },
+    {
+      "title": " Road Generation",
+      "link": "SettingUpVehicleSimul",
+      "children": null
+    },
+    {
+      "title": " Standard Asset Library",
+      "link": "SettingUpVehicleSimul",
+      "children": null
+    },
+    {
+      "title": " Vehicular Simulation",
+      "link": "SettingUpVehicleSimul",
+      "children": null
+    },
+    {
+      "title": " Pedestrian Simulation",
+      "link": "SettingUpVehicleSimul",
+      "children": null
+    },
+    {
+      "title": " Glossary",
+      "link": "SettingUpVehicleSimul",
+      "children": null
+    }
+  ]
+};
---
+++ docdata/toc.js
@@ -0,0 +1,152 @@
+toc = {
+  "title": " Fore Documentation (1.0.1)",
+  "link": "ForeDocumentation",
+  "children": [
+    {
+        "title": " System Requirements",
+        "link": "SystemRequirements",
+        "children": null
+    },
+    {
+      "title": " Setting up the project",
+      "link": "SettingUp",
+      "children": [
+        {
+          "title": " Installing required assets",
+          "link": "SettingUpVehicleSimul",
+          "children": [
+            {
+              "title": " Project Repository",
+              "link": "SettingUpVehicleSimul",
+              "children": null
+            },
+            {
+              "title": " Unity Asset Store",
+              "link": "SettingUpVehicleSimul",
+              "children": null
+            },
+            {
+              "title": " Other custom deletions",
+              "link": "SettingUpVehicleSimul",
+              "children": null
+            }
+          ]
+        },
+        {
+          "title": " Required Software",
+          "link": "SettingUpVehicleSimul",
+          "children": [
+            {
+              "title": " Rhinoceros 6",
+              "link": "SettingUpVehicleSimul",
+              "children": null
+            },
+            {
+              "title": " Photoshop",
+              "link": "SettingUpVehicleSimul",
+              "children": null
+            },
+            {
+              "title": " Open Street Map",
+              "link": "SettingUpVehicleSimul",
+              "children": null
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "title": " Preparing the environment",
+      "link": "SettingUpVehicleSimul",
+      "children": [
+        {
+          "title": " Importing of models",
+          "link": "SettingUpVehicleSimul",
+          "children": null
+        },
+        {
+          "title": " Basic Configurations",
+          "link": "SettingUpVehicleSimul",
+          "children": [
+            {
+              "title": " Probuilderize",
+              "link": "SettingUpVehicleSimul",
+              "children": null
+            },
+            {
+              "title": " Setting Global Parameters",
+              "link": "SettingUpVehicleSimul",
+              "children": null
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "title": " Procedural Building",
+      "link": "D_ProceduralBuildingIntro",
+      "children": [
+        {
+          "title": " Pre-requisites",
+          "link": "SettingUpVehicleSimul",
+          "children": null
+        },
+        {
+          "title": " Download OSM Data",
+          "link": "SettingUpVehicleSimul",
+          "children": null
+        },
+        {
+          "title": " Internalise Data",
+          "link": "SettingUpVehicleSimul",
+          "children": null
+        },
+        {
+          "title": " Customise Building Height",
+          "link": "SettingUpVehicleSimul",
+          "children": null
+        },
+        {
+          "title": " Customise Building Texture",
+          "link": "SettingUpVehicleSimul",
+          "children": null
+        },
+        {
+          "title": " Customise Texture Mapping",
+          "link": "SettingUpVehicleSimul",
+          "children": null
+        },
+        {
+          "title": " Importing to Unity",
+          "link": "SettingUpVehicleSimul",
+          "children": null
+        }
+      ]
+    },
+    {
+      "title": " Road Generation",
+      "link": "SettingUpVehicleSimul",
+      "children": null
+    },
+    {
+      "title": " Standard Asset Library",
+      "link": "SettingUpVehicleSimul",
+      "children": null
+    },
+    {
+      "title": " Vehicle Simulation",
+      "link": "SettingUpVehicleSimul",
+      "children": null
+    },
+    {
+      "title": " Pedestrian Simulation",
+      "link": "SettingUpVehicleSimul",
+      "children": null
+    },
+    {
+      "title": " Glossary",
+      "link": "SettingUpVehicleSimul",
+      "children": null
+    }
+  ]
+};
